8423a29dec750ea686639c8fa35cd9ce75f93a81
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-06-26 05:49:29 +0900

    feat: Added Nav component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,35 @@ import React,{ Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Books from './containers/Books';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import CreateBook from './containers/CreateBook';
+import Nav from './components/Nav';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      pathname: '',
+    };
+
+    this.notifyPathname = this.notifyPathname.bind(this);
+  }
+
+  notifyPathname(pathname) {
+    this.setState({
+      pathname: pathname,
+    });
   }
 
   render() {
     return (
       <Router>
         <div className="App">
+          <Nav
+            notifyPathname={this.notifyPathname}
+            pathname={this.state.pathname}
+          />
           <Switch>
             <Route path="/"
                     exact
