6d1c6c0a4ebd69880b3d201bea5c62570c346798
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-06-29 06:01:59 +0900

    feat: Added Action and Reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import { fetchBooks } from './actions/book.actions.js';
+
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history'
+import { createBrowserHistory } from 'history';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+store.dispatch(fetchBooks());
 
 export const history = createBrowserHistory({forceRefresh: true});
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ frontend/src/reducers/bookReducer.js
@@ -0,0 +1,29 @@
+import {
+    ADD_BOOK_ERROR,
+    ADD_BOOK_LOADING,
+    ADD_BOOK_SUCCESS,
+    DELETE_BOOK_ERROR,
+    DELETE_BOOK_LOADING,
+    DELETE_BOOK_SUCCESS,
+    EDIT_BOOK_ERROR,
+    EDIT_BOOK_LOADING,
+    EDIT_BOOK_SUCCESS,
+    FETCH_BOOKS_ERROR,
+    FETCH_BOOKS_LOADING,
+    FETCH_BOOKS_SUCCESS
+} from '../actions/types'
+
+const defaultState = {
+    books: [],
+    error: null,
+    isLoading: false,
+};
+
+const bookReducer = (state = defaultState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default bookReducer;
