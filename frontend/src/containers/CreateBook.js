9c5ddff419d5a35210b5028ac0af2023534c22fa
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-07-01 07:39:27 +0900

    added connect createbook to redux

--- frontend/src/containers/CreateBook.js
+++ frontend/src/containers/CreateBook.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
+import { createBook } from '../actions/book.actions';
+import { connect } from 'react-redux';
 import './CreateBook.css'
 
 class CreateBook extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            id: 0,
+            title: '',
+            author: '',
+            year: '',
+        };
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.onAdd(this.state);
+    }
+
+    handleOnValueChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
     }
 
     render() {
         return (
             <div className="create-book">
-                <form>
+                <form onSubmit={this.handleSubmit.bind(this)}>
                     <div className="form-group">
                         <input
                             type="text"
                             className="form-control"
                             name="title"
                             placeholder="Enter Title"
+                            onChange={this.handleOnValueChange.bind(this)}
                         />
                     </div>
                     <div className="form-group">
@@ -24,6 +45,7 @@ class CreateBook extends Component {
                             className="form-control"
                             name="author"
                             placeholder="Enter Author"
+                            onChange={this.handleOnValueChange.bind(this)}
                         />
                     </div>
                     <div className="form-group">
@@ -32,6 +54,7 @@ class CreateBook extends Component {
                             className="form-control"
                             name="year"
                             placeholder="Enter Year Published"
+                            onChange={this.handleOnValueChange.bind(this)}
                         />
                     </div>
                     <div className="form-group">
@@ -44,4 +67,18 @@ class CreateBook extends Component {
     }
 }
 
-export default CreateBook;
+const mapStateToProps = (state) => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onAdd: (book) => {
+            dispatch(createBook(book));
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateBook);
